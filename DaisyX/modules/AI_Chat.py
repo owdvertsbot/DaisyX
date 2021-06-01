dc2257212c92f1d46c19064b173a5cb9017b2962
owdvertsbot <84202766+owdvertsbot@users.noreply.github.com>
2021-06-01 11:03:22 +0530

    Update AI_Chat.py

--- DaisyX/modules/AI_Chat.py
+++ DaisyX/modules/AI_Chat.py
@@ -385,7 +385,7 @@ async def inuka(client, message):
 
 __help__ = """
 <b> Chatbot </b>
-DAISY AI 3.0 IS THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES
+DR STRANGE AI 3.0 IS THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES
 
  - /chatbot [ON/OFF]: Enables and disables AI Chat mode (EXCLUSIVE)
  - /chatbot EN : Enables English only chatbot
